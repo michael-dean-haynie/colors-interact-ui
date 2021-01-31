384f6cea88068a18ea8eecb291bbd260b611ed41
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2021-01-30 23:56:30 -0500

    add hello world connection to api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DevelopmentApiService } from './services/development-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'colors-interact-ui';
+
+  private subscriptions: Subscription[] = [];
+
+  constructor(
+    private developmentApiService: DevelopmentApiService
+  ) { }
+
+
+  ngOnInit(): void {
+    this.subscriptions.push(this.developmentApiService.helloworld().subscribe());
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
